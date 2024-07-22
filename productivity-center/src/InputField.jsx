2aead26caac44e8851656e3de432170d771c3229
raymondumbas <raymond.umbas@gmail.com>
2024-07-21 20:23:11 -0700

    implemented basic components

--- productivity-center/src/InputField.jsx
+++ productivity-center/src/InputField.jsx
@@ -1,11 +1,31 @@
-export default function InputField(props) {
+import { forwardRef } from "react";
+
+/* INPUT FIELD COMPONENT
+    Props:
+    - name (string) = text label for the input
+    -type (string) = what type of input
+
+    Ref:
+    - passed from parent to retrieve value of the field
+*/
+
+
+const InputField = forwardRef(function InputField(props,ref) {
+
+    const handleChange = (e) => {
+        if (props.type === "checkbox") {
+            ref.current.checked = e.target.checked;
+        } else {
+            ref.current.value = e.target.value;
+        }
+    };
 
     //Textarea    
     if(props.type == "textarea"){
         return(
             <>
                 <label htmlFor = {props.name}>{props.name}</label>
-                <textarea name = {props.name} id = {props.name}></textarea>
+                <textarea name = {props.name} id = {props.name} onChange = {handleChange} ref = {ref} ></textarea>
             </>
         )
 
@@ -15,9 +35,11 @@ export default function InputField(props) {
         return (
             <>
                 <label htmlFor = {props.name}>{props.name}</label>
-                <input type = {props.type} name = {props.name} id = {props.name}></input>
+                <input type = {props.type} name = {props.name} id = {props.name} onChange = {handleChange} ref = {ref}></input>
             </>
         )
     }
 
-}
+});
+
+export default InputField;
---
+++ productivity-center/src/OutputDisplay.jsx
@@ -0,0 +1,5 @@
+export default function OutputDisplay(props){
+    return( 
+        <div>{props.text}</div>
+    )
+}
