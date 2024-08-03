4941dcdf6378edca6e51d454d88a03e1c33a95af
raymondumbas <raymond.umbas@gmail.com>
2024-08-03 02:07:36 -0700

    implemented deleting habits and logs

--- productivity-center/src/HabitListCard.jsx
+++ productivity-center/src/HabitListCard.jsx
@@ -9,19 +9,62 @@ export default function HabitListCard(props) {
     console.log(displayHabit)
 
     const deleteHabit = () =>{
-        console.log("trash");
+
+        //Remove from habitList
+        const habitList = JSON.parse(localStorage.getItem("habitList"));
+        console.log("before",habitList)
+        const index = habitList.indexOf(displayHabit);
+        if(index > -1){
+            habitList.splice(index,1);
+        }
+        console.log("after",habitList)
+        localStorage.setItem("habitList", JSON.stringify(habitList));
+        localStorage.removeItem(displayHabit);
+        setView("gallery");
     }
 
-    const deleteLog = () => {
+    const deleteLog = (currentHabit, targetLog) => {
+        console.log(currentHabit)
+        console.log(targetLog)
+        
+        // Find index of targetLog in habit log
+        const index = currentHabit.log.indexOf(targetLog);
+
+        // If targetLog found, then remove it
+        if(index > -1){
+            currentHabit.log.splice(index,1)
+        }
+
+        // Remove metric value from total
+        const oldTotal = currentHabit.total;
+
+        if(currentHabit.metric == "time"){
+            const startTime = new Date(targetLog[0]);
+            const endTime = new Date(targetLog[1]);
+
+            const logTime = (endTime.getTime() - startTime.getTime())/1000;
+            currentHabit.total = oldTotal - logTime;
+        }
+        else if(currentHabit.metric == "count"){
+            
+            currentHabit.total = oldTotal - targetLog[1];
+        }
+
+        //Update habit
+        localStorage.setItem(displayHabit, JSON.stringify(currentHabit))
+
+        
 
     }
 
     if(view == "gallery"){
-    const showNewHabit = () =>{
-        props.setPrevPage("habits");
-        props.setPage("newHabit")
 
-    }
+        const showNewHabit = () =>{
+            props.setPrevPage("habits");
+            props.setPage("newHabit")
+
+        }
+
         const habitList = JSON.parse(localStorage.getItem("habitList"));
         const habitElements = habitList.map((habit,index) => {
             const currentHabit = JSON.parse(localStorage.getItem(habit));
@@ -42,6 +85,7 @@ export default function HabitListCard(props) {
 
         return(
             <>
+                <h1>Your Habits</h1>
                 <Button text = "+" onclick = {showNewHabit} />
                 {habitElements}
             </>
@@ -51,21 +95,33 @@ export default function HabitListCard(props) {
     else if(view == "details"){
         const currentHabit = JSON.parse(localStorage.getItem(displayHabit));
         const logElements = currentHabit.log.map((log,index) => {
-
-            let logTime;
-            let endTime;
-            const startTime = (new Date(log[0])).getTime();
-
-            if(log[1] != null){
-                endTime = (new Date(log[1])).getTime();
-                logTime = (endTime - startTime)/1000;
+            let logUnits;
+            let zeroLogItem;
+            let oneLogItem;
+            if(currentHabit.metric == "time"){
+    
+                zeroLogItem = (new Date(log[0])).getTime();
+    
+                if(log[1] != null){
+                    oneLogItem = (new Date(log[1])).getTime();
+                    logUnits = (oneLogItem - zeroLogItem)/1000;
+                }
             }
+
+            else if (currentHabit.metric == "count"){
+                zeroLogItem = log[0];
+                oneLogItem = "";
+                logUnits = log[1];
+            }
+
             return(
                 <div key = {index}>
-                        <span>{startTime}</span>
-                        <span>{endTime}</span>
-                        <span>{logTime}</span>
-                        <span>🗑️</span>
+
+                    <span>{zeroLogItem}</span>
+                    <span>{oneLogItem}</span>
+                    <span>{logUnits}</span>
+                    <Button text = "🗑️" onclick = {() => deleteLog(currentHabit,log)}/>
+
                 </div>
             )    
         });
@@ -74,7 +130,7 @@ export default function HabitListCard(props) {
                <div>
                     <Button text = "<-" onclick = {() => setView("gallery")}/>
                     <span>{displayHabit}</span>
-                    <Button text = "🗑️" onclick = {deleteHabit}/>
+                    <Button text = "🗑️" onclick = {() => deleteHabit()}/>
                     {logElements}
                 </div> 
             </>
